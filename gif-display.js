c13b7f1bcabe598f5a473e92430cc63e4d6f7113
Mike Wallace <mryanwallace@gmail.com>
2019-12-09 17:53:30 -0800

    Implemented giphy API call to fetch an image and display it in a div

--- gif-display.js
+++ gif-display.js
@@ -3,8 +3,51 @@
 // 
 // See HW4 writeup for more hints and details.
 class GifDisplay {
-  constructor() {
-    // TODO(you): Implement the constructor and add fields as necessary.
+  constructor(container) {
+    // Bindings
+    this.onResolved = this.onResolved.bind(this);
+    // Members
+    this.theme = null;
+    this.images = [];
+    // DOM Nodes
+    this.gif = document.createElement('div');
+    this.gif.id = 'gif';
+    container.appendChild(this.gif);
+
   }
-  // TODO(you): Add methods as necessary.
+  
+  setTheme(theme) {
+    this.theme = theme;
+  }
+
+  getImages(theme = this.theme) {
+    const url = 'https://api.giphy.com/v1/gifs/search?q=';
+    const limit = '&limit=25';
+    const rating = '&rating=g';
+    // Hello github!
+    const api_key = '&api_key=';
+    const query = encodeURIComponent(theme);
+    fetch(url + query + api_key + limit + rating)
+      .then(this.onSuccess, this.onFailure)
+      .then(this.onResolved);
+  }
+
+  onSuccess(response) {
+    return response.json();
+  }
+
+  onFailure(error) {
+    console.log(error);
+  }
+
+  onResolved(resource) {
+    this.images = resource['data'];
+    this.setImage(this.images[0]);
+    console.log(this.images);
+  }
+
+  setImage(obj) {
+    const src = 'url(' + obj.images.downsized.url + ')';
+    this.gif.style.backgroundImage = src;
+  }
 }
